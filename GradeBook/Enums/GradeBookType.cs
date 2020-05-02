0e85e8682bc5ed93932b861f6f82428153a87e3c
ayub174 <kadir.ayub@gmail.com>
2020-05-02 02:31:04 +0200

    .

--- GradeBook/Enums/GradeBookType.cs
+++ GradeBook/Enums/GradeBookType.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GradeBook.Enums
 {
-    //enum holds a constant value.
+   
     public enum GradeBookType
     {
         Standard,
--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,13 +7,24 @@ namespace GradeBook.GradeBooks
     public class RankedGradeBook : BaseGradeBook
     {
 
-
         public RankedGradeBook(string name) : base(name)
         {
-
+                
             this.Type = Enums.GradeBookType.Ranked;
-
+            
         }
+
+
+        //Create an override for the get grade method
+        public override char GetLetterGrade(double averageGrade)
+        {
+            
+            if(Students.Capacity < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            return 'F';
+        }
+
     }
 }
-
